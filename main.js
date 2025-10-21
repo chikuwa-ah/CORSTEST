b4c380f4a6cec2e22f199594cbfeb2829877e77a
chikuwa-ah <chikuwah21@gmail.com>
2025-10-21 23:05:35 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxQ85fneT9IVM3Yv-Xdu3WVEyzFQCGt7_neMn8cUzdoxyt324M4hpkjiIsB5USA7BGu/exec";
+
+async function fetchData() {
+    const res = await fetch(GAS_URL);
+    const data = await res.json();
+    console.log(data);
+}
+
+async function addData() {
+    const newItem = { name: "Alice", passWord: 'pass' };
+    const res = await fetch(GAS_URL, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem)
+    });
+    console.log("データ送信完了");
+}
+
+
+document.addEventListener('click', (e) => {
+    if (e.target.textContent === 'GET') {
+        fetchData();
+    } else {
+        addData();
+    }
+})
